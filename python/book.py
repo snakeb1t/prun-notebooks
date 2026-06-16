99b0c5515b2a56f948ae5b81155f23dc4feaf932
trainwreck <m4lk4v@gmail.com>
2026-06-16 11:55:27 -0600

    add script to record order book every hour for last 7 days

--- python/book.py
+++ python/book.py
@@ -1,8 +1,40 @@
 import prun_data_frames as prundf
 import polars as ps
+import sqlite3 as sql
+import os
+from pathlib import Path
+
+script_dir = Path(__file__).resolve().parent
+os.chdir(script_dir)
+
+relative = Path("../../prundb/prundb.db")
+dbfile = relative.resolve()
+
+connection_uri = f"sqlite:///{dbfile}"
+print(connection_uri)
 
 if __name__ == "__main__":
+
     orders = prundf.PrunOrders()
     bids = prundf.PrunBids()
 
-    print(orders.source_df)
+    orders.source_df.write_database(
+        table_name="orders",
+        connection=connection_uri,
+        if_table_exists="append"  # Options: 'fail', 'replace', 'append'
+    )
+
+    bids.source_df.write_database(
+        table_name="bids",
+        connection=connection_uri,
+        if_table_exists="append"  # Options: 'fail', 'replace', 'append'
+    )
+
+    connection = sql.connect(dbfile)
+
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM orders WHERE timestamp < datetime('now', '-7 days');")
+    cursor.execute("DELETE FROM bids WHERE timestamp < datetime('now', '-7 days');")
+    connection.commit()
+
+    connection.close()
