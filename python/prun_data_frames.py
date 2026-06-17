a467e506aa90a43d6693ad29afa7845ecae55ed3
trainwreck <m4lk4v@gmail.com>
2026-06-16 21:04:15 -0600

    begin to make recipe dataframe

--- python/prun_data_frames.py
+++ python/prun_data_frames.py
@@ -1,5 +1,29 @@
 import polars as ps
 from datetime import datetime
+from enum import Enum, auto
+
+class CX(Enum):
+    CI1 = auto()
+    CI2 = auto()
+    NC1 = auto()
+    NC2 = auto()
+    AI1 = auto()
+    IC1 = auto()
+
+class Currency(Enum):
+    CIS = auto()
+    AIC = auto()
+    ICA = auto()
+    NCC = auto()
+
+CXtoCurrency = {
+    CX.CI1: Currency.CIS,
+    CX.CI2: Currency.CIS,
+    CX.NC1: Currency.NCC,
+    CX.NC2: Currency.NCC,
+    CX.AI1: Currency.AIC,
+    CX.IC1: Currency.ICA
+}
 
 class lazyproperty:
     def __init__(self, func):
@@ -20,14 +44,26 @@ class PrunFrame:
     def source_df(self):
         return ps.read_csv(self.source, infer_schema_length=self.schema_len)
 
+class PrunBuildings(PrunFrame):
+    source = "https://rest.fnar.net/csv/buildings"
+    costs_source = "https://rest.fnar.net/csv/buildingcosts"
+    recipes_source = "https://rest.fnar.net/csv/buildingrecipes"
+    workforces_source = "https://rest.fnar.net/csv/buildingworkforces"
+    @lazyproperty
+    def costs_df(self):
+        return ps.read_csv(self.costs_source, infer_schema_length=self.schema_len)
+    @lazyproperty
+    def recipes_df(self):
+        return ps.read_csv(self.recipes_source, infer_schema_length=self.schema_len)
+    @lazyproperty
+    def workforces_df(self):
+        return ps.read_csv(self.workforces_source, infer_schema_length=self.schema_len)
+
 class PrunPrices(PrunFrame):
     source = "https://rest.fnar.net/csv/prices"
 
 class PrunMaterials(PrunFrame):
     source = "https://rest.fnar.net/csv/materials"
-
-class PrunRecipes(PrunFrame):
-    source = "https://rest.fnar.net/csv/buildingrecipes"
 
 class PrunOrders(PrunFrame):
     source = "https://rest.fnar.net/csv/orders"
