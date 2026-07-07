75dc0be91bfb20bae490bc22878c2b25a51e08b4
trainwreck <m4lk4v@gmail.com>
2026-07-07 10:00:19 -0600

    more layout

--- python/book_viewer.py
+++ python/book_viewer.py
@@ -1,7 +1,7 @@
 from textual.app import App, ComposeResult
 from textual.screen import Screen
 from textual.containers import HorizontalGroup
-from textual.widgets import Placeholder, Header, Footer, Label, Select
+from textual.widgets import Placeholder, Header, Footer, Label, Select, Static
 import prun_data_frames as pdf
 
 mats = pdf.PrunMaterials().source_df["Ticker"].to_list()
@@ -27,9 +27,11 @@ class BookViewerApp(App):
         yield AskTable("Ask Table")
         yield BidTable("Bid Table")
         with HorizontalGroup():
-            yield Select(id="material", options=mat_options, value="SF")
-            yield Select(id="cx", options=cx_options, value="CI1")
-            yield TimeLabel(id="time")
+            yield Select(id="material", options=mat_options, value="SF", prompt="Material", name="Material")
+            yield Select(id="cx", options=cx_options, value="CI1", prompt="CX", name="CX")
+            yield Label("<", id="left")
+            yield TimeLabel("The Time", id="time")
+            yield Label(">", id="right")
         yield Footer(id="Footer")
 
 if __name__ == "__main__":
